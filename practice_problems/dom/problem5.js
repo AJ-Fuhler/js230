c126ffd91d28dd6c84c5293c2ca323ebb3b24f58
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-18 16:13:57 -0500

    added additional solution to problem5

--- practice_problems/dom/problem5.js
+++ practice_problems/dom/problem5.js
@@ -23,3 +23,22 @@ function color(elements) {
 function getAllChildrenOf(parents) {
   return parents.flatMap(({children}) => [...children]);
 }
+
+// or
+
+function colorGeneration(generation) {
+  let currentGeneration = 1;
+  let currentGenElements = [...document.body.children];
+
+  while (currentGeneration < generation) {
+    let children = [];
+    currentGenElements.forEach(child => {
+      children = children.concat([...child.children]);
+    });
+
+    currentGenElements = children;
+    currentGeneration += 1;
+  }
+
+  currentGenElements.forEach(element => element.classList.add('generation-color'));
+}
