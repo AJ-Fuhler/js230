17b7b142f00d67887be19d819b4598d46f2a7afd
AJ Fuhler <aart.fuhler@gmail.com>
2025-09-20 11:21:03 -0500

    implemented all requirements. Ready for code review

--- practice_problems/form/problem1/problem1.js
+++ practice_problems/form/problem1/problem1.js
@@ -1,13 +1,85 @@
-let firstNameInput = document.getElementById('first-name');
-let lastNameInput = document.getElementById('last-name');
-let emailInput = document.getElementById('email');
-let passwordInput = document.getElementById('password');
-let phoneInput = document.getElementById('phone');
-let submitBtn = document.querySelector('button');
+let form = document.querySelector('form');
+let inputs = [...form.querySelectorAll('input')];
+let genericError = form.firstElementChild;
 
-firstNameInput.addEventListener('blur', event => {
-  if (firstNameInput.value.length < 1) {
-    let textNode = document.createTextNode("Please Enter a valid Email.");
-    textNode.classList.add('error');
+const INVALID_EMAIL_MESSAGE = "Please Enter a Valid Email";
+const INVALID_PASSWORD_MESSAGE = "Password must be at least 10 characters long.";
+const INVALID_PHONE_MESSAGE = "Please Enter a Valid Phone Number.";
+const INVALID_FIRST_NAME_MESSAGE = "First Name is a required field.";
+const INVALID_LAST_NAME_MESSAGE = "Last Name is a required field.";
+
+function getErrorMessage(id) {
+  switch (id) {
+    case 'first-name':
+      return INVALID_FIRST_NAME_MESSAGE;
+    case 'last-name':
+      return INVALID_LAST_NAME_MESSAGE;
+    case 'email':
+      return INVALID_EMAIL_MESSAGE;
+    case 'password':
+      return INVALID_PASSWORD_MESSAGE;
+    case 'phone':
+      return INVALID_PHONE_MESSAGE;
+  }
+}
+
+function handleValidityBlur(event) {
+  let input = event.target;
+  let id = input.id;
+
+  if (!input.checkValidity()) {
+    input.classList.add('error-input')
+    let errorParagraph = document.createElement('p');
+    errorParagraph.textContent = getErrorMessage(id);
+    errorParagraph.classList.add('error-message');
+    input.parentNode.appendChild(errorParagraph);
+  }
+
+}
+
+function handleFocus(event) {
+  let input = event.target;
+  input.classList.remove('error-input');
+  let errorMessage = input.parentNode.querySelector('p');
+
+  if (errorMessage) {
+    errorMessage.remove();
+  }
+}
+
+function anyErrors() {
+  return inputs.some(input => !input.checkValidity());
+}
+
+function showGenericError() {
+  genericError.classList.remove('hide');
+}
+
+function hideGenericError() {
+  genericError.classList.add('hide');
+}
+
+document.addEventListener('blur', event => {
+  if (event.target.tagName === 'INPUT') {
+    handleValidityBlur(event);
+    if (!anyErrors()) {
+      hideGenericError();
+    }
+  }
+}, true);
+
+document.addEventListener('focus', event => {
+  if (event.target.tagName === 'INPUT') {
+    handleFocus(event);
+  }
+}, true);
+
+form.addEventListener('submit', event => {
+  if (anyErrors()) {
+    event.preventDefault();
+    showGenericError();
+    inputs.forEach(input => input.dispatchEvent(new Event('blur')));
+  } else {
+    hideGenericError();
   }
 })
